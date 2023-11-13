e8098d8e0df20d89d60d9b5421747497daffaa16
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2023-11-13 07:33:45 +0300

    Правка названия базы в сущности User

--- prerevolutionary-tinder-server/prerevolutionary-tinder-server-app/src/main/java/ru/liga/model/User.java
+++ prerevolutionary-tinder-server/prerevolutionary-tinder-server-app/src/main/java/ru/liga/model/User.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
