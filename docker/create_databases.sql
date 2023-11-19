336d102ed8f13a2632fd8377b33f2892a6ac2040
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2023-11-19 21:45:57 +0300

    Переделал docker-compose.yml добавил скрипт создания баз данных в одном образе.

---
+++ docker/create_databases.sql
@@ -0,0 +1,5 @@
+-- Создаем базу данных tinder_server_database
+CREATE DATABASE tinder_server_database WITH OWNER = "user";
+
+-- Создаем базу данных tinder_client_database
+CREATE DATABASE tinder_client_database WITH OWNER = "user";
