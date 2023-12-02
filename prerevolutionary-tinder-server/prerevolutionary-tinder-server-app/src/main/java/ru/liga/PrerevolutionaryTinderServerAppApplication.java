7b956167827d4ae7103d442a5eec8e333046347e
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2023-12-02 20:41:31 +0300

    Выенс customConversionService в файл конфигураций

--- prerevolutionary-tinder-server/prerevolutionary-tinder-server-app/src/main/java/ru/liga/PrerevolutionaryTinderServerAppApplication.java
+++ prerevolutionary-tinder-server/prerevolutionary-tinder-server-app/src/main/java/ru/liga/PrerevolutionaryTinderServerAppApplication.java
@@ -2,35 +2,10 @@ package ru.liga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ConversionServiceFactoryBean;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.converter.Converter;
-import ru.liga.dto.converter.FavouriteEntityToDtoConverter;
-import ru.liga.dto.converter.MatchingProfileToDtoWithImageConverter;
-import ru.liga.dto.converter.ProfileEntityToProfileDtoConverter;
-import ru.liga.dto.converter.ProfileToDtoWithImageConverter;
-
-import java.util.HashSet;
 
 @SpringBootApplication
 public class PrerevolutionaryTinderServerAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(PrerevolutionaryTinderServerAppApplication.class, args);
-    }
-
-    @Bean(name = "customConversionService")
-    public ConversionService getConversionService(ApplicationContext context) {
-
-        final ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
-        final HashSet<Converter> converters = new HashSet<>();
-        converters.add(context.getBean(ProfileEntityToProfileDtoConverter.class));
-        converters.add(context.getBean(FavouriteEntityToDtoConverter.class));
-        converters.add(context.getBean(ProfileToDtoWithImageConverter.class));
-        converters.add(context.getBean(MatchingProfileToDtoWithImageConverter.class));
-        bean.setConverters(converters);
-        bean.afterPropertiesSet();
-        return bean.getObject();
     }
 }
---
+++ prerevolutionary-tinder-server/prerevolutionary-tinder-server-app/src/main/java/ru/liga/config/AppConfig.java
@@ -0,0 +1,32 @@
+package ru.liga.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import ru.liga.dto.converter.FavouriteEntityToDtoConverter;
+import ru.liga.dto.converter.MatchingProfileToDtoWithImageConverter;
+import ru.liga.dto.converter.ProfileEntityToProfileDtoConverter;
+import ru.liga.dto.converter.ProfileToDtoWithImageConverter;
+
+import java.util.HashSet;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "customConversionService")
+    public ConversionService getConversionService(ApplicationContext context) {
+        final ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+        final HashSet<Converter> converters = new HashSet<>();
+        converters.add(context.getBean(ProfileEntityToProfileDtoConverter.class));
+        converters.add(context.getBean(FavouriteEntityToDtoConverter.class));
+        converters.add(context.getBean(ProfileToDtoWithImageConverter.class));
+        converters.add(context.getBean(MatchingProfileToDtoWithImageConverter.class));
+        bean.setConverters(converters);
+        bean.afterPropertiesSet();
+        return bean.getObject();
+    }
+
+}
